5b33b1e026939080c4d13dfbf435882890afeb8b
Ayslan <anossetti@indracompany.com>
2020-02-17 15:43:59 -0300

    Alterado idade

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -3,7 +3,7 @@ public class HelloWord {
 
 	public static void main(String[] args) {
 		
-    System.out.println ("Olá, Mundo! Ayslan");
+    System.out.println ("Olá, Mundo! Ayslan, 26");
     
 	}
 
